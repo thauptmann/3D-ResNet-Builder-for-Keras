a06c912b683b49f033675b795796175b7a80de1a
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-01-20 16:12:38 +0100

    Combine also resnet bottleneck layers and se-resnet layers

--- three_d_resnet/layers.py
+++ three_d_resnet/layers.py
@@ -2,27 +2,13 @@ from tensorflow import keras
 import tensorflow as tf
 
 
-class CustomConv3D(keras.layers.Layer):
-    def __init__(self, kernel_number, kernel_size, regularizer, strides=1, use_bn=False, padding='valid',  **kwargs):
-        super(CustomConv3D, self).__init__(**kwargs)
-        self.custom_conv_3d = keras.Sequential()
-        self.custom_conv_3d.add(keras.layers.Conv3D(kernel_number, kernel_size, strides, padding=padding,
-                                                    use_bias=not use_bn, kernel_regularizer=regularizer))
-        if use_bn:
-            self.custom_conv_3d.add(keras.layers.BatchNormalization())
-        self.custom_conv_3d.add(keras.layers.ReLU())
-
-    def call(self, inputs, training=None):
-        return self.custom_conv_3d(inputs)
-
-
 class ResidualBlock(keras.layers.Layer):
-    def __init__(self, kernel_number, kernel_size, regularizer, squeeze_and_excitation=False, **kwargs):
+    def __init__(self, kernel_number, kernel_size, regularizer=None, squeeze_and_excitation=False, **kwargs):
         super(ResidualBlock, self).__init__(**kwargs)
         self.squeeze_and_excitation = squeeze_and_excitation
         self.resnet_block = keras.Sequential(
             [
-                CustomConv3D(kernel_number, kernel_size, 1, padding='same', use_bn=True),
+                CustomConv3D(kernel_number, kernel_size, regularizer, 1, padding='same', use_bn=True),
                 keras.layers.Conv3D(kernel_number, kernel_size, padding='same', use_bias=False,
                                     kernel_regularizer=regularizer),
                 keras.layers.BatchNormalization()
@@ -45,11 +31,11 @@ class ResidualBlock(keras.layers.Layer):
 
 
 class ResidualConvBlock(keras.layers.Layer):
-    def __init__(self, kernel_number, kernel_size, strides=1, squeeze_and_excitation=False, **kwargs):
+    def __init__(self, kernel_number, kernel_size, regularizer=None, strides=1, squeeze_and_excitation=False, **kwargs):
         super(ResidualConvBlock, self).__init__(**kwargs)
         self.resnet_conv_block = keras.Sequential(
             [
-                CustomConv3D(kernel_number, kernel_size, strides=strides, padding='same', use_bn=True),
+                CustomConv3D(kernel_number, kernel_size, regularizer, strides=strides, padding='same', use_bn=True),
                 keras.layers.Conv3D(kernel_number, kernel_size, padding='same', use_bias=False,
                                     kernel_regularizer=keras.regularizers.l2()),
                 keras.layers.BatchNormalization()
@@ -79,33 +65,40 @@ class ResidualConvBlock(keras.layers.Layer):
 
 
 class ResidualBottleneckBlock(keras.layers.Layer):
-    def __init__(self, kernel_number, kernel_size, **kwargs):
+    def __init__(self, kernel_number, kernel_size, regularizer=None, squeeze_and_excitation=False, **kwargs):
         super(ResidualBottleneckBlock, self).__init__(**kwargs)
         self.resnet_bottleneck_block = keras.Sequential(
             [
-                CustomConv3D(kernel_number, 1, use_bn=True),
-                CustomConv3D(kernel_number, kernel_size, padding='same', use_bn=True),
+                CustomConv3D(kernel_number, regularizer, 1, use_bn=True),
+                CustomConv3D(kernel_number, kernel_size, regularizer, padding='same', use_bn=True),
                 keras.layers.Conv3D(kernel_number * 4, 1, use_bias=False),
                 keras.layers.BatchNormalization()
             ]
         )
         self.relu = keras.layers.ReLU()
+        if squeeze_and_excitation:
+            self.se_path = SqueezeAndExcitationPath(kernel_number)
 
     def call(self, inputs, training=None, **kwargs):
         intermediate_output = self.resnet_bottleneck_block(inputs)
+
+        if self.squeeze_and_excitation:
+            weights = self.se_path(inputs)
+            intermediate_output = intermediate_output * weights
+
         output_sum = tf.add(intermediate_output, inputs)
         output = self.relu(output_sum)
         return output
 
 
 class ResidualConvBottleneckBlock(keras.layers.Layer):
-    def __init__(self, kernel_number, kernel_size, strides=1, **kwargs):
+    def __init__(self, kernel_number, kernel_size, regularizer=None, squeeze_and_excitation=False, strides=1, **kwargs):
         super(ResidualConvBottleneckBlock, self).__init__(**kwargs)
         self.resnet_conv_bottleneck_block = keras.Sequential(
             [
-                CustomConv3D(kernel_number, 1, strides=strides, use_bn=True),
-                CustomConv3D(kernel_number, kernel_size, padding='same', use_bn=True),
-                keras.layers.Conv3D(kernel_number * 4, 1, use_bias=False, kernel_regularizer=keras.regularizers.l2()),
+                CustomConv3D(kernel_number, 1, regularizer, strides=strides, use_bn=True),
+                CustomConv3D(kernel_number, kernel_size, regularizer, padding='same', use_bn=True),
+                keras.layers.Conv3D(kernel_number * 4, 1, use_bias=False, kernel_regularizer=regularizer),
                 keras.layers.BatchNormalization()
             ]
         )
@@ -115,10 +108,17 @@ class ResidualConvBottleneckBlock(keras.layers.Layer):
             keras.layers.BatchNormalization()
         ]
         )
+        if squeeze_and_excitation:
+            self.se_path = SqueezeAndExcitationPath(kernel_number)
 
     def call(self, inputs, training=None, **kwargs):
         intermediate_output = self.resnet_conv_bottleneck_block(inputs)
         shortcut = self.shortcut_conv(inputs)
+
+        if self.squeeze_and_excitation:
+            weights = self.se_path(inputs)
+            intermediate_output = intermediate_output * weights
+
         output_sum = tf.add(intermediate_output, shortcut)
         output = self.relu(output_sum)
         return output
@@ -143,30 +143,16 @@ class SqueezeAndExcitationPath(keras.layers.Layer):
         return reshaped_weights
 
 
-class SqueezeExcitationResidualConvBlock(keras.layers.Layer):
-    def __init__(self, kernel_number, kernel_size, strides=1, **kwargs):
-        super(SqueezeExcitationResidualConvBlock, self).__init__(**kwargs)
-        self.resnet_conv_block = keras.Sequential(
-            [
-                CustomConv3D(kernel_number, kernel_size, strides=strides, padding='same', use_bn=True),
-                keras.layers.Conv3D(kernel_number, kernel_size, padding='same', use_bias=False,
-                                    kernel_regularizer=keras.regularizers.l2()),
-                keras.layers.BatchNormalization()
-            ]
-        )
-        self.relu = keras.layers.ReLU()
-        self.shortcut_conv = keras.Sequential(
-            [
-                keras.layers.Conv3D(kernel_number, 1, strides=strides, kernel_regularizer=keras.regularizers.l2()),
-                keras.layers.BatchNormalization()
-            ])
-        self.se_path = SqueezeAndExcitationPath(kernel_number)
+class CustomConv3D(keras.layers.Layer):
+    def __init__(self, kernel_number, kernel_size, regularizer=None, strides=1, use_bn=False, padding='valid',
+                 **kwargs):
+        super(CustomConv3D, self).__init__(**kwargs)
+        self.custom_conv_3d = keras.Sequential()
+        self.custom_conv_3d.add(keras.layers.Conv3D(kernel_number, kernel_size, strides, padding=padding,
+                                                    use_bias=not use_bn, kernel_regularizer=regularizer))
+        if use_bn:
+            self.custom_conv_3d.add(keras.layers.BatchNormalization())
+        self.custom_conv_3d.add(keras.layers.ReLU())
 
     def call(self, inputs, training=None):
-        intermediate_output = self.resnet_conv_block(inputs)
-        shortcut = self.shortcut_conv(inputs)
-        weights = self.se_path(inputs)
-        weighted_output = weights * intermediate_output
-        output_sum = tf.add(weighted_output, shortcut)
-        output = self.relu(output_sum)
-        return output
+        return self.custom_conv_3d(inputs)
