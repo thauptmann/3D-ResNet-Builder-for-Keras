7db959ad0d2fab74f9603c08796431625a1a2e21
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-01-20 16:59:40 +0100

    Adding resnet build methods

--- three_d_resnet/resnet_builder.py
+++ three_d_resnet/resnet_builder.py
@@ -1,22 +1,37 @@
-import tensorflow as tf
 import three_D_resnet
 
 
-def build_three_d_resnet(regularization=None, use_bottleneck_layers=False, use_squeeze_and_excitation_layers=False):
-    pass
+def build_three_d_resnet(input_shape, output_shape, repetitions, output_activation, regularizer=None,
+                         squeeze_and_excitation=False, use_bottleneck=False, kernel_size=3):
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, repetitions, output_activation,
+                                                  regularizer, squeeze_and_excitation, use_bottleneck, kernel_size)
 
 
-def build_three_d_resnet_18():
-    pass
+def build_three_d_resnet_18(input_shape, output_shape, output_activation, regularizer=None,
+                            squeeze_and_excitation=False):
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (2, 2, 2, 2), output_activation,
+                                                  regularizer, squeeze_and_excitation)
 
 
-def build_three_d_resnet_34():
-    pass
+def build_three_d_resnet_34(input_shape, output_shape, output_activation, regularizer=None,
+                            squeeze_and_excitation=False):
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 4, 6, 3), output_activation,
+                                                  regularizer, squeeze_and_excitation)
 
 
-def build_three_d_resnet_50():
-    pass
+def build_three_d_resnet_50(input_shape, output_shape, output_activation, regularizer=None,
+                            squeeze_and_excitation=False):
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 4, 6, 3), output_activation,
+                                                  regularizer, squeeze_and_excitation, use_bottleneck=True)
 
 
-def build_three_d_resnet_152():
-    pass
+def build_three_d_resnet_102(input_shape, output_shape, output_activation, regularizer=None,
+                             squeeze_and_excitation=False):
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 4, 23, 3), output_activation,
+                                                  regularizer, squeeze_and_excitation, use_bottleneck=True)
+
+
+def build_three_d_resnet_152(input_shape, output_shape, output_activation, regularizer=None,
+                             squeeze_and_excitation=False):
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 8, 36, 3), output_activation,
+                                                  regularizer, squeeze_and_excitation, use_bottleneck=True)
--- three_d_resnet/three_D_resnet.py
+++ three_d_resnet/three_D_resnet.py
@@ -3,12 +3,12 @@ from layers import ResidualBlock, ResidualBottleneckBlock, ResidualConvBlock, Re
 
 
 class ThreeDConvolutionResNet(keras.Model):
-    def __init__(self, input_shape, output_shape, kernel_numbers, repetitions, output_activation, regularizer=None,
-                 mean=0, std=1, use_bottleneck=False, kernel_size=3, squeeze_and_excitation=False):
+    def __init__(self, input_shape, output_shape, repetitions, output_activation, regularizer=None,
+                 squeeze_and_excitation=False, use_bottleneck=False, kernel_size=3):
         super(ThreeDConvolutionResNet, self).__init__()
         if use_bottleneck:
             residual_conv_block = ResidualConvBottleneckBlock
-            residual_block = ResidualConvBlock
+            residual_block = ResidualBottleneckBlock
         else:
             residual_conv_block = ResidualConvBlock
             residual_block = ResidualBlock
@@ -22,7 +22,8 @@ class ThreeDConvolutionResNet(keras.Model):
 
         resnet_body = keras.Sequential()
         strides = 1
-        for kernel_number, repetition in zip(repetitions, kernel_numbers):
+        kernel_number = 64
+        for repetition in repetitions:
             for i in range(repetition):
                 if i == 0:
                     resnet_body.add(residual_conv_block(kernel_number, kernel_size, regularizer,
@@ -31,6 +32,7 @@ class ThreeDConvolutionResNet(keras.Model):
                     resnet_body.add(residual_block(kernel_number, kernel_size, regularizer,
                                                    squeeze_and_excitation, strides=1))
                 strides = 2
+                kernel_number *= 2
 
         # fix resnet tail
         resnet_tail = keras.Sequential(
@@ -41,109 +43,6 @@ class ThreeDConvolutionResNet(keras.Model):
             ]
         )
         self.resnet = keras.Sequential([resnet_head, resnet_body, resnet_tail])
-
-    def call(self, inputs, training=None, **kwargs):
-        return self.resnet(inputs)
-
-
-# variable 2 layer blocks resnet-18
-#        ResidualBlock(64, 3)
-#        ResidualBlock(64, 3)
-
-#        ResidualConvBlock(128, 3, 2)
-#        ResidualBlock(128, 3)
-
-#        ResidualConvBlock(256, 3, 2)
-#        ResidualBlock(256, 3)
-
-#        ResidualConvBlock(512, 3, 2)
-#        ResidualBlock(512, 3)
-
-class ThreeDConvolutionResNet34(keras.Model):
-    def __init__(self, width, height, frames, channels, mean, std, output=1):
-        super(ThreeDConvolutionResNet34, self).__init__()
-        input_shape = (frames, width, height, channels)
-        self.resnet = keras.Sequential([
-            keras.layers.experimental.preprocessing.Rescaling(scale=1 / std, offset=-mean / std,
-                                                              input_shape=input_shape),
-            # fix resnet head
-            keras.layers.Conv3D(64, 7, 2, use_bias=False, kernel_regularizer=keras.regularizers.l2()),
-            keras.layers.BatchNormalization(),
-            keras.layers.ReLU(),
-            keras.layers.MaxPool3D(3, 2),
-
-            # variable 2 layer blocks
-            ResidualBlock(64, 3),
-            ResidualBlock(64, 3),
-            ResidualBlock(64, 3),
-
-            ResidualConvBlock(128, 3, 2),
-            ResidualBlock(128, 3),
-            ResidualBlock(128, 3),
-            ResidualBlock(128, 3),
-
-            ResidualConvBlock(256, 2),
-            ResidualBlock(256, 3),
-            ResidualBlock(256, 3),
-            ResidualBlock(256, 3),
-            ResidualBlock(256, 3),
-            ResidualBlock(256, 3),
-
-            ResidualConvBlock(512, 3, 2),
-            ResidualBlock(512, 3),
-            ResidualBlock(512, 3),
-
-            # fix resnet tail
-            keras.layers.GlobalAvgPool3D(),
-            keras.layers.Flatten(),
-            keras.layers.Dense(output),
-        ]
-        )
-
-    def call(self, inputs, training=False, **kwargs):
-        return self.resnet(inputs)
-
-
-class ThreeDConvolutionResNet50(keras.Model):
-    def __init__(self, width, height, frames, channels, mean, std, output=1):
-        super(ThreeDConvolutionResNet50, self).__init__()
-        input_shape = (frames, width, height, channels)
-        self.resnet = keras.Sequential([
-            keras.layers.experimental.preprocessing.Rescaling(scale=1 / std, offset=-mean / std,
-                                                              input_shape=input_shape),
-            # fix resnet head
-            keras.layers.Conv3D(64, 7, 2, use_bias=False, kernel_regularizer=keras.regularizers.l2()),
-            keras.layers.BatchNormalization(),
-            keras.layers.ReLU(),
-            keras.layers.MaxPool3D(3, 2),
-
-            # variable 3 layer blocks
-            ResidualConvBottleneckBlock(64, 3, 2),
-            ResidualBottleneckBlock(64, 3),
-            ResidualBottleneckBlock(64, 3),
-
-            ResidualConvBottleneckBlock(128, 3, 2),
-            ResidualBottleneckBlock(128, 3),
-            ResidualBottleneckBlock(128, 3),
-            ResidualBottleneckBlock(128, 3),
-
-            ResidualConvBottleneckBlock(256, 3, 2),
-            ResidualBottleneckBlock(256, 3),
-            ResidualBottleneckBlock(256, 3),
-            ResidualBottleneckBlock(256, 3),
-            ResidualBottleneckBlock(256, 3),
-            ResidualBottleneckBlock(256, 3),
-
-            ResidualConvBottleneckBlock(512, 3, 2),
-            ResidualBottleneckBlock(512, 3),
-            ResidualBottleneckBlock(512, 3),
-
-            # fix resnet tail
-            keras.layers.GlobalAvgPool3D(),
-            keras.layers.Flatten(),
-            keras.layers.Dense(output),
-        ]
-        )
 
     def call(self, inputs, training=False, **kwargs):
         return self.resnet(inputs)
