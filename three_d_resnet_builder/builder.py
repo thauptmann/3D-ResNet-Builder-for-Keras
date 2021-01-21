9def831a7415686d7f54103040c63bf867024b23
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-01-21 22:01:51 +0100

    Refactor

--- three_d_resnet_builder/builder.py
+++ three_d_resnet_builder/builder.py
@@ -1,4 +1,4 @@
-import three_D_resnet
+from . import three_D_resnet
 
 
 def build_three_d_resnet(input_shape, output_shape, repetitions, output_activation, regularizer=None,
@@ -9,29 +9,29 @@ def build_three_d_resnet(input_shape, output_shape, repetitions, output_activati
 
 def build_three_d_resnet_18(input_shape, output_shape, output_activation, regularizer=None,
                             squeeze_and_excitation=False):
-    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (2, 2, 2, 2), output_activation,
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, output_activation, (2, 2, 2, 2),
                                                   regularizer, squeeze_and_excitation)
 
 
 def build_three_d_resnet_34(input_shape, output_shape, output_activation, regularizer=None,
                             squeeze_and_excitation=False):
-    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 4, 6, 3), output_activation,
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, output_activation, (3, 4, 6, 3),
                                                   regularizer, squeeze_and_excitation)
 
 
 def build_three_d_resnet_50(input_shape, output_shape, output_activation, regularizer=None,
                             squeeze_and_excitation=False):
-    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 4, 6, 3), output_activation,
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, output_activation, (3, 4, 6, 3),
                                                   regularizer, squeeze_and_excitation, use_bottleneck=True)
 
 
 def build_three_d_resnet_102(input_shape, output_shape, output_activation, regularizer=None,
                              squeeze_and_excitation=False):
-    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 4, 23, 3), output_activation,
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, output_activation,(3, 4, 23, 3),
                                                   regularizer, squeeze_and_excitation, use_bottleneck=True)
 
 
 def build_three_d_resnet_152(input_shape, output_shape, output_activation, regularizer=None,
                              squeeze_and_excitation=False):
-    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, (3, 8, 36, 3), output_activation,
+    return three_D_resnet.ThreeDConvolutionResNet(input_shape, output_shape, output_activation, (3, 8, 36, 3),
                                                   regularizer, squeeze_and_excitation, use_bottleneck=True)
