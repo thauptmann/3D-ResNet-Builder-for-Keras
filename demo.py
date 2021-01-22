735a4b665885b45786270f8bd3e9b9aff7718e43
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-01-22 09:38:44 +0100

    Refactor and add top-1 and top-5 accuracy

--- demo.py
+++ demo.py
@@ -6,33 +6,40 @@ import tensorflow as tf
 def train_resnet():
     seed_value = 5
     tf.random.set_seed(seed_value)
-    config = tfds.download.DownloadConfig(verify_ssl=False)
-    (train_dataset, test_dataset), ds_info = tfds.load("ucf101", split=['train', 'test'], with_info=True,
-                                                       shuffle_files=True,
-                                                       download_and_prepare_kwargs={"download_config": config},
-                                                       batch_size=10)
-    input_shape = ds_info.features['video'].shape
-    output_shape = ds_info.features['label'].num_classes
-    train_dataset = train_dataset.map(lambda sample: normalize_img( sample),
-                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-    test_dataset = test_dataset.map(lambda sample: normalize_img(sample),
-                                    num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    test_dataset = test_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    train_dataset, test_dataset, info = load_ucf101()
+    input_shape = info.features['video'].shape
+    output_shape = info.features['label'].num_classes
 
     resnet_18 = three_d_resnet_builder.build_three_d_resnet_18(input_shape, output_shape, 'softmax')
     resnet_18.compile(
         optimizer=tf.keras.optimizers.Adam(0.001),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-        metrics=[tf.keras.metrics.SparseCategoricalCrossentropy()],
+        metrics=[
+            tf.keras.metrics.SparseTopKCategoricalAccuracy(k=1),
+            tf.keras.metrics.SparseTopKCategoricalAccuracy(k=2),
+        ],
     )
 
     resnet_18.fit(train_dataset, epochs=6, validation_data=test_dataset)
 
 
+def load_ucf101():
+    config = tfds.download.DownloadConfig(verify_ssl=False)
+    (train_dataset, test_dataset), ds_info = tfds.load("ucf101", split=['train', 'test'], with_info=True,
+                                                       shuffle_files=True, batch_size=10,
+                                                       download_and_prepare_kwargs={"download_config": config})
+    train_dataset = train_dataset.map(lambda sample: normalize_img(sample),
+                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    test_dataset = test_dataset.map(lambda sample: normalize_img(sample),
+                                    num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    test_dataset = test_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    return train_dataset, test_dataset, ds_info
+
+
 def normalize_img(sample):
-    """Normalizes images: `uint8` -> `float32`."""
     video = sample['video']
+    """Normalizes images: `uint8` -> `float32`."""
     video = tf.cast(video, tf.float32) / 255.
     return video, sample['label']
 
