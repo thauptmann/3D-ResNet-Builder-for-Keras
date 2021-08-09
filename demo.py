5e15c0a261d269e89c917253cd1bdc2dce9aba6d
Tony Hauptmann <65155653+thauptmann@users.noreply.github.com>
2021-08-09 14:21:17 +0200

    Reduce batch size

--- demo.py
+++ demo.py
@@ -11,7 +11,7 @@ mixed_precision.set_global_policy('mixed_float16')
 
 def train_resnet(use_squeeze_and_excitation, depth, kernel_type):
     seed_value = 5
-    batch_size = 24
+    batch_size = 16
     epochs = 200
     scale = 2
     number_of_frames = 100
@@ -45,20 +45,20 @@ def train_resnet(use_squeeze_and_excitation, depth, kernel_type):
 
 def generate_network_architecture(depth, input_shape, output_shape, use_squeeze_and_excitation):
     if depth == 34:
-        resnet = three_d_resnet_builder.build_three_d_resnet_34(input_shape, output_shape, 'softmax',
-                                                                use_squeeze_and_excitation)
+        resnet = three_d_resnet_builder.build_three_d_resnet_34(input_shape, output_shape, 'softmax', 'l2',
+                                                                squeeze_and_excitation=use_squeeze_and_excitation)
     elif depth == 50:
-        resnet = three_d_resnet_builder.build_three_d_resnet_50(input_shape, output_shape, 'softmax',
-                                                                use_squeeze_and_excitation)
+        resnet = three_d_resnet_builder.build_three_d_resnet_50(input_shape, output_shape, 'softmax', 'l2',
+                                                                squeeze_and_excitation=use_squeeze_and_excitation)
     elif depth == 102:
-        resnet = three_d_resnet_builder.build_three_d_resnet_102(input_shape, output_shape, 'softmax',
-                                                                 use_squeeze_and_excitation)
+        resnet = three_d_resnet_builder.build_three_d_resnet_102(input_shape, output_shape, 'softmax', 'l2',
+                                                                 squeeze_and_excitation=use_squeeze_and_excitation)
     elif depth == 152:
-        resnet = three_d_resnet_builder.build_three_d_resnet_152(input_shape, output_shape, 'softmax',
-                                                                 use_squeeze_and_excitation)
+        resnet = three_d_resnet_builder.build_three_d_resnet_152(input_shape, output_shape, 'softmax', 'l2',
+                                                                 squeeze_and_excitation=use_squeeze_and_excitation)
     else:
-        resnet = three_d_resnet_builder.build_three_d_resnet_18(input_shape, output_shape, 'softmax',
-                                                                use_squeeze_and_excitation)
+        resnet = three_d_resnet_builder.build_three_d_resnet_18(input_shape, output_shape, 'softmax', 'l2',
+                                                                squeeze_and_excitation=use_squeeze_and_excitation)
     return resnet
 
 
@@ -97,7 +97,7 @@ def preprocess_image(sample, number_of_frames):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--use_squeeze_and_excitation', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--use_squeeze_and_excitation', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--depth', default=18, type=int, choices=[18, 34, 50, 102, 152])
     args = parser.parse_args()
     train_resnet(args.use_squeeze_and_excitation, args.depth, None)
